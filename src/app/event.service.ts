7caf6dea26328bf8de6da0e4ba352356909baa59
Brenden <brendensosnader@gmail.com>
2018-04-02 20:49:45 -0400

    connect service to django

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,19 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 import { Event } from './event';
 import { EVENTS } from './mock-events';
 
-
+private eventUrl = 'http://ec2-18-188-184-129.us-east-2.compute.amazonaws.com:8000/events/';
 
 @Injectable()
 export class EventService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+  private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
 
   getEvents(): Observable<Event[]> {
-    return of(EVENTS);
+    return this.http.get<Event[]>(this.eventUrl)
+    .pipe(
+      tap(events => this.log(`fetched events`)),
+      catchError(this.handleError('getEvents', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
